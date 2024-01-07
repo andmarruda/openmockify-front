e39a1ce0e8d2173bcc6f096fd0d4d25d57f90384
Anderson <andmarruda@gmail.com>
2024-01-06 23:32:34 -0300

    preparing edit subdomain layout

--- src/pages/Subdomain/Edit.tsx
+++ src/pages/Subdomain/Edit.tsx
@@ -2,10 +2,15 @@ import { useTranslation } from "react-i18next";
 import { BreadcrumbList } from "../../types/BreadcrumbList";
 import Breadcrumb from "../../components/Breadcrumb";
 import { useParams } from "react-router-dom";
+import Input from "../../components/Input";
+import useInput from "../../hooks/useInput";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 
 const Edit = () => {
     const { t } = useTranslation();
     const { id } = useParams();
+    const subdomainInput = useInput('');
 
     const breadcrumbList : BreadcrumbList = [
         {
@@ -23,6 +28,12 @@ const Edit = () => {
     return (
         <>
             <Breadcrumb list={breadcrumbList} />
+            <form>
+                <Input type="text" name="subdomain" id="subdomain" className="form-control" label="subdomain" {...subdomainInput} />
+                <div className="mb-3">
+                    <button type="submit" className="btn btn-outline-primary"><FontAwesomeIcon icon={faFloppyDisk} /> { t('save') }</button>
+                </div>
+            </form>
         </>
     )
 }
